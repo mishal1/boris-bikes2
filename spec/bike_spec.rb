8e7df8879fd9f2f3703176d8f2bb557f09b4a373
Mishal Islam <mishal.islam01@gmail.com>
2015-02-02 12:56:14 +0000

    changes before each to let

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,14 +5,17 @@ describe Bike do
   before(:each) do
     @bike = Bike.new
   end
+
+  let(:bike) {Bike.new}
+  let(:break_bike) {bike.break!}
     
   it 'should not be broken when initialized' do
-    expect(@bike.broken?).to be false
+    expect(bike.broken?).to be false
   end
 
   it 'should be able to break' do
-    @bike.break!
-    expect(@bike.broken?).to be true
+    break_bike
+    expect(bike.broken?).to be true
   end
 
 end
